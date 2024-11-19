063a78d97520b7fd95b3c0844bd9fe1bb18141d9
Rachit Garg <rachitgarg900@gmail.com>
2024-11-19 01:02:40 +0100

    Update main.kt

--- Kotlin_Newbie_to_Pro/20_Classes/main.kt
+++ Kotlin_Newbie_to_Pro/20_Classes/main.kt
@@ -1,3 +1,5 @@
 fun main() {
+    // The area of 2, 4 and 6 (Values from the video) will always be 0 using Heron's formula
+    // Hence, using valid values for a triangle
     val triangle = Triangle(3.0, 4.0, 5.0)
 }
